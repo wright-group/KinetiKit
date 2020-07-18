0bc0e00e8670d101197cbefd5e0428e90129724d
naspitha <spitha@wisc.edu>
2020-07-18 14:58:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
         "numpy>=1.15.0",
         "scipy",
     ],
-    version='0.1.4',
+    version='0.1.5',
     description="Tools for comparing and kinetically simulating time-resolved data",
 )
